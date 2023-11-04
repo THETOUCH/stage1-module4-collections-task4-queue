814e998302ffe8e63771bc3c26554507f4e003d2
THETOUCH <super.ilgiznex@gmail.com>
2023-11-04 22:36:58 +0600

    123

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,22 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Queue<Integer> dishes = Stream.iterate(1, i -> i + 1)
+                .limit(numberOfDishes)
+                .collect(Collectors.toCollection(LinkedList::new));
+        List<Integer> dishesToTry = new ArrayList<>();
+
+        while (!dishes.isEmpty()) {
+            for (int i = 1; i < everyDishNumberToEat; i++) {
+                dishes.add(dishes.poll());
+            }
+            dishesToTry.add(dishes.poll());
+        }
+        return dishesToTry;
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,23 @@
 package com.epam.collections.queue;
 
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        PriorityQueue<String> mergedCollection = new PriorityQueue<>(new StringsQueueComparator());
+        List<String> commonList = new ArrayList<>(firstList);
+        commonList.addAll(secondList);
+        mergedCollection.addAll(commonList);
+        return mergedCollection;
+    }
+}
+class StringsQueueComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        if (o1.equals(o2)) {
+            return 0;
+        }
+        return -o1.compareTo(o2);
     }
 }
